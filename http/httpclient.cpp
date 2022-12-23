44c8327bd41b218ab052a2d774bee55b4cf0b1e9
Ogunleye Pius <piusayowale@gmail.com>
2022-12-23 10:51:34 +0100

    check error conditions

--- http/httpclient.cpp
+++ http/httpclient.cpp
@@ -12,44 +12,73 @@ std::string getName()
 
 tcp::resolver::results_type HttpClient::resolve(std::string const& hostname)
 {
+    boost::system::error_code error;
     tcp::resolver resolver{ this->context };
-    return resolver.resolve(hostname, "https");
+    auto ret = resolver.resolve(hostname, "https", error);
+    if (error.failed()) {
+        std::cout << error.message() << "\n";
+        return {};
+    }
+    return ret;
+
 }
 
 tcp::socket HttpClient::connect(std::string const& hostname)
 {
+    boost::system::error_code code;
     tcp::socket socket{ this->context};
-    asio::connect(socket, resolve(hostname));
+    asio::connect(socket, resolve(hostname), code);
+    if (code.failed()) {
+        socket.close();
+        //return socket;
+    }
     return socket;
 }
 
 std::unique_ptr<ssl::stream<tcp::socket>>& HttpClient::connect_ssl(std::string const& hostname)
 {
+
+    boost::system::error_code error;
     stream_ptr = boost::make_unique<ssl::stream<tcp::socket>>(connect(hostname), ssl_context);
 
     boost::certify::set_server_hostname(*stream_ptr, hostname);
     boost::certify::sni_hostname(*stream_ptr, hostname);
 
 
-    stream_ptr->handshake(ssl::stream_base::handshake_type::client);
+    stream_ptr->handshake(ssl::stream_base::handshake_type::client, error);
     
+    if (error.failed()) {
+        std::cout << error.message();
+        stream_ptr = nullptr;
+        return stream_ptr;
+    }
+
     return stream_ptr;
 }
 
 http::response<http::string_body> HttpClient::get_stream(std::string uri)
 {
+    boost::system::error_code code;
     http::request<http::empty_body> request;
     request.method(http::verb::get);
     request.target(uri);
     //std::cout << uri << "\n";
     request.keep_alive(false);
     request.set(http::field::host, host_);
-    http::write(*stream_ptr, request);
+    http::write(*stream_ptr, request, code);
+    if (code.failed()) {
+        std::cout << code.message() << "\n";
+        return {};
+    }
 
     http::response<http::string_body> response;
     
     beast::flat_buffer buffer;
-    http::read(*stream_ptr, buffer, response);
+    http::read(*stream_ptr, buffer, response, code);
+    if (code.failed()) {
+        std::cout << code.message() << "\n";
+        return {};
+    }
 
     return response;
 }
@@ -57,9 +86,14 @@ http::response<http::string_body> HttpClient::get_stream(std::string uri)
 int HttpClient::get(std::string target, std::string& out)
 {
   
-    connect_ssl(this->host_);
+    if (connect_ssl(this->host_) == nullptr) {
+        return -1;
+    }
     std::cout << this->host_;
     auto response = get_stream(target);
+    if (!response.payload_size().has_value()) {
+        return -1;
+    }
     out = std::move(response.body());
 
     boost::system::error_code ec;
--- nairaland/nairaland.cpp
+++ nairaland/nairaland.cpp
@@ -17,97 +17,114 @@ using namespace std;
 
 std::map<std::string, int> visited;
 
-static void search_for_links(GumboNode* node, std::vector<std::string>& urls) {
-    if (node->type != GUMBO_NODE_ELEMENT) {
-        return;
-    }
-    GumboAttribute* href;
-    if (node->v.element.tag == GUMBO_TAG_A &&
-        (href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
-      //  std::cout << href->value << std::endl;
-        urls.push_back(std::string(href->value));
-    }
 
-    GumboVector* children = &node->v.element.children;
-    for (unsigned int i = 0; i < children->length; ++i) {
-        search_for_links(static_cast<GumboNode*>(children->data[i]), urls);
-    }
+void processText(GumboNode* node) {
+
+
+	if (node->type == GUMBO_NODE_TEXT) {
+		std::cout << node->v.text.text << "\n";
+	}
+
+
+}
+
+static void search_for_links(GumboNode* node, std::vector<std::string>& urls) {
+	if (node->type != GUMBO_NODE_ELEMENT) {
+		return processText(node);
+	}
+	GumboAttribute* href;
+	GumboAttribute* cls;
+	if (node->v.element.tag == GUMBO_TAG_A &&
+		(href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
+		//  std::cout << href->value << std::endl;
+		urls.push_back(std::string(href->value));
+	}
+	if (node->v.element.tag == GUMBO_TAG_TABLE) {
+
+	}
+
+	GumboVector* children = &node->v.element.children;
+	for (unsigned int i = 0; i < children->length; ++i) {
+		search_for_links(static_cast<GumboNode*>(children->data[i]), urls);
+	}
 }
 
 std::stack<std::string> linkQueue;
 
-void httpProcessor(HttpClient& client, std::string &target) {
+void httpProcessor(HttpClient& client, std::string& target) {
 
 
-   // std::cout << "Targeted " << target << "\n";
-    visited[target]++;
-    std::string out;
-    std::cout << client.get(target, out);
-    std::vector<std::string> urls;
-    GumboOutput* output = gumbo_parse(out.c_str());
-    search_for_links(output->root, urls);
-    std::string t;
+	// std::cout << "Targeted " << target << "\n";
+	visited[target]++;
+	std::string out;
+	if (client.get(target, out) < 0) {
+		return;
+	}
+	std::vector<std::string> urls;
+	GumboOutput* output = gumbo_parse(out.c_str());
+	search_for_links(output->root, urls);
+	std::string t;
 
-    while (!linkQueue.empty()) {
+	while (!linkQueue.empty()) {
 
-       // std::cout << "================= " << linkQueue.size() << "========\n";
-        t = linkQueue.top(); linkQueue.pop();
+		// std::cout << "================= " << linkQueue.size() << "========\n";
+		t = linkQueue.top(); linkQueue.pop();
 
-        std::cout << t << "\n";
-        
-       
-        for (auto url : urls) {
-            
-            Poco::URI link(url);
-            // std::cout << link.getHost() << "\n";
-            std::string host = link.getHost();
-            if (host == NAIRALAND || host == "") {
-                // std::cout << url << "\n";
-                std::string path = link.getPath();
-                if (visited[path] < 1) {
-                    //std::cout << " Detected " << path << "\n";
-                    linkQueue.push(path);
-                    
-                }
-            }
+		std::cout << t << "\n";
 
-            urls.pop_back();
-            
-    }
-     
-       // httpProcessor(client, t);
-        if (output != nullptr) {
-             gumbo_destroy_output(&kGumboDefaultOptions, output);
-             output = nullptr;
-        }
-        
-        httpProcessor(client, t);
-    
-    }
-    
-    
+
+		for (auto url : urls) {
+
+			Poco::URI link(url);
+			// std::cout << link.getHost() << "\n";
+			std::string host = link.getHost();
+			if (host == NAIRALAND || host == "") {
+				// std::cout << url << "\n";
+				std::string path = link.getPath();
+				if (visited[path] < 1) {
+					//std::cout << " Detected " << path << "\n";
+					linkQueue.push(path);
+
+				}
+			}
+
+			urls.pop_back();
+
+		}
+
+		// httpProcessor(client, t);
+		if (output != nullptr) {
+			gumbo_destroy_output(&kGumboDefaultOptions, output);
+			output = nullptr;
+		}
+
+		httpProcessor(client, t);
+
+	}
+
+
 }
 
 int main()
 {
 
-    asio::io_context ctx;
-    ssl::context ssl_ctx{ ssl::context::tls_client };
-    ssl_ctx.set_verify_mode(ssl::context::verify_peer | ssl::context::verify_fail_if_no_peer_cert);
-    ssl_ctx.set_default_verify_paths();
+	asio::io_context ctx;
+	ssl::context ssl_ctx{ ssl::context::tls_client };
+	ssl_ctx.set_verify_mode(ssl::context::verify_peer | ssl::context::verify_fail_if_no_peer_cert);
+	ssl_ctx.set_default_verify_paths();
 
-    boost::certify::enable_native_https_server_verification(ssl_ctx);
+	boost::certify::enable_native_https_server_verification(ssl_ctx);
 
-    HttpClient client{ ssl_ctx, ctx, "www.nairaland.com" };
-    visited["/register"]++;
-    visited["/login"]++;
-    visited[""]++;
-    visited[" "]++;
-    std::string target = "/";
-    linkQueue.push("/");
-    httpProcessor(client, target);
+	HttpClient client{ ssl_ctx, ctx, "www.nairaland.com" };
+	visited["/register"]++;
+	visited["/login"]++;
+	visited[""]++;
+	visited[" "]++;
+	std::string target = "/";
+	linkQueue.push("/");
+	httpProcessor(client, target);
 
-  
+
 
 	return 0;
 }
